3dc690300726ff6ffd9e28c20831ce51c361007b
VUTRIEUBINH <100052453+VUTRIEUBINH@users.noreply.github.com>
2023-11-11 08:49:13 +0700

    chương 7
    c7

---
+++ 7.4.py
@@ -0,0 +1,28 @@
+
+x = 16
+y = 4
+
+print((x, y))
+
+equivelence = x == y
+print("x = -y is", equivelence)
+
+equivelence = x != y
+print("x != y is", equivelence)
+
+equivelence = x > y
+print("x > y is", equivelence)
+
+x = 8
+y = 9
+
+print((x, y))
+
+equivelence = x >= y
+print("x >= y is", equivelence)
+
+equivelence = x < y
+print("x < y is", equivelence)
+
+equivelence = x <= y
+print("x <= y is", equivelence)
